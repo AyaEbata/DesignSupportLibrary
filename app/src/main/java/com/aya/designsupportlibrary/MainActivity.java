bad2a1932b375deeeb3a66e4ae23cf5548d9cd72
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-11-23 16:04:36 +0900

    create Floating Action Button

--- app/src/main/java/com/aya/designsupportlibrary/MainActivity.java
+++ app/src/main/java/com/aya/designsupportlibrary/MainActivity.java
@@ -1,11 +1,15 @@
 package com.aya.designsupportlibrary;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 
+/**
+ * TextInputLayout.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,22 +17,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setLoginButton();
+    }
+
+    private void setLoginButton() {
         Button loginButton = (Button) findViewById(R.id.login_button);
         loginButton.setOnClickListener(view -> {
-            checkValidate(R.id.id_input_layout, R.id.id_text, "id");
-            checkValidate(R.id.pass_input_layout, R.id.pass_text, "Password");
+            boolean isId = checkValidate(R.id.id_input_layout, R.id.id_text, "id");
+            boolean isPass = checkValidate(R.id.pass_input_layout, R.id.pass_text, "Password");
+            if (isId && isPass) {
+                startActivity(new Intent(MainActivity.this, FabActivity.class));
+            }
         });
     }
 
-    private void checkValidate(int textInputLayoutId, int editTextId, String text) {
+    private boolean checkValidate(int textInputLayoutId, int editTextId, String text) {
         TextInputLayout textInputLayout = (TextInputLayout) findViewById(textInputLayoutId);
         EditText editText = (EditText) findViewById(editTextId);
 
         if (editText.getText().length() == 0) {
             textInputLayout.setErrorEnabled(true);
             textInputLayout.setError(getString(R.string.error, text));
+            return false;
         } else {
             textInputLayout.setErrorEnabled(false);
+            return true;
         }
 
     }
