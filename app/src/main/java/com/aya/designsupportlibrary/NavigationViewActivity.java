deecf0e903f51d0646f0a3151194824be036b0d4
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-12-16 20:44:12 +0900

    add 三 button

--- app/src/main/java/com/aya/designsupportlibrary/NavigationViewActivity.java
+++ app/src/main/java/com/aya/designsupportlibrary/NavigationViewActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 /**
  * Navigation View.
@@ -24,13 +25,21 @@ public class NavigationViewActivity extends AppCompatActivity {
     private void setToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(v -> {
+            Log.d("nav", "nav");
+            // ここに三本線をクリックした時に、Drawerが開くような処理を入れる
+        });
     }
 
     private void setDrawerToggle() {
         DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
         ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
         drawerLayout.setDrawerListener(drawerToggle);
-        drawerToggle.setDrawerIndicatorEnabled(true);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        drawerToggle.syncState();
     }
 
 }
