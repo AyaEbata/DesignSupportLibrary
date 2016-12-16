87ac923615d3c43c49c9ebe03e85ad90c3df327c
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-12-16 14:57:32 +0900

    add menu button action

--- app/src/main/java/com/aya/designsupportlibrary/BottomNavigationViewActivity.java
+++ app/src/main/java/com/aya/designsupportlibrary/BottomNavigationViewActivity.java
@@ -1,7 +1,9 @@
 package com.aya.designsupportlibrary;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Bottom Navigation  View.
@@ -13,6 +15,27 @@ public class BottomNavigationViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation_view);
+
+        onClickedMenu();
+    }
+
+    private void onClickedMenu() {
+        TextView text = (TextView) findViewById(R.id.text);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.action_search:
+                    text.setText(R.string.menu_search);
+                    break;
+                case R.id.action_add:
+                    text.setText(R.string.menu_add);
+                    break;
+                case R.id.action_settings:
+                    text.setText(R.string.menu_settings);
+                    break;
+            }
+            return true;
+        });
     }
 
 }
