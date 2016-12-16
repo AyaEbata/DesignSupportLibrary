7616d2ed7ad41a97a248639cd100712b4da7f7c4
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-12-16 19:22:55 +0900

    change ActionBar to Toolbar

--- app/src/main/java/com/aya/designsupportlibrary/NavigationViewActivity.java
+++ app/src/main/java/com/aya/designsupportlibrary/NavigationViewActivity.java
@@ -1,7 +1,10 @@
 package com.aya.designsupportlibrary;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Navigation View.
@@ -13,6 +16,21 @@ public class NavigationViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_view);
+
+        setToolbar();
+        setDrawerToggle();
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void setDrawerToggle() {
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.setDrawerListener(drawerToggle);
+        drawerToggle.setDrawerIndicatorEnabled(true);
     }
 
 }
